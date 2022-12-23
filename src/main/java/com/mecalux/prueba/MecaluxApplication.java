253a873dbd775a195c5ac9cfaccda69783aafe69
Manel Reus Giner <manel.Giner@serviceware-se.com>
2022-12-23 23:16:36 +0100

    Initial commit

---
+++ src/main/java/com/mecalux/prueba/MecaluxApplication.java
@@ -0,0 +1,13 @@
+package com.mecalux.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecaluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecaluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mecalux/prueba/MecaluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mecalux.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecaluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
