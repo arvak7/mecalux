6e4645c59840a20238adbd7868c27b83448fa623
Manel Reus Giner <manel.Giner@serviceware-se.com>
2023-01-23 15:44:27 +0100

    Improve restful naming

--- src/main/java/com/mecalux/prueba/warehouse/WarehouseController.java
+++ src/main/java/com/mecalux/prueba/warehouse/WarehouseController.java
@@ -9,11 +9,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,7 +21,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/warehouse")
+@RequestMapping("/api/v1/warehouses")
 @Tag(name = "Warehouse")
 @RequiredArgsConstructor
 public class WarehouseController {
@@ -37,15 +37,15 @@ public class WarehouseController {
     }
 
     @Nonnull
-    @GetMapping
-    @Timed(value = "warehouse.read.time", description = "time to read a new warehouse", percentiles = {0.5, 0.9})
-    protected ResponseEntity<Warehouse> read(@RequestParam UUID uuid) {
+    @GetMapping("/{uuid}")
+    @Timed(value = "warehouse.read.time", description = "time to read a warehouse", percentiles = {0.5, 0.9})
+    protected ResponseEntity<Warehouse> read(@PathVariable UUID uuid) {
         return ResponseEntity.ok().body(warehouseService.readWarehouse(uuid).orElseThrow(NotFoundException::new));
     }
 
     @Nonnull
-    @GetMapping("all")
-    @Timed(value = "warehouse.read.time", description = "time to read a new warehouse", percentiles = {0.5, 0.9})
+    @GetMapping
+    @Timed(value = "warehouses.read.time", description = "time to read a warehouse", percentiles = {0.5, 0.9})
     protected ResponseEntity<List<Warehouse>> readAll() {
         return ResponseEntity.ok().body(warehouseService.findAll().orElseThrow(NotFoundException::new));
     }
@@ -60,9 +60,9 @@ public class WarehouseController {
 
 
     @Nonnull
-    @DeleteMapping
+    @DeleteMapping("/{uuid}")
     @Timed(value = "warehouse.delete.time", description = "time to delete a warehouse", percentiles = {0.5, 0.9})
-    protected ResponseEntity<String> delete(@RequestParam UUID uuid) {
+    protected ResponseEntity<String> delete(@PathVariable UUID uuid) {
         Boolean deleted = warehouseService.deleteWarehouse(uuid);
         if (!deleted) throw new NotFoundException(uuid.toString());
         return ResponseEntity.ok().body(String.format("Your Warehouse with uuid %s has been deleted", uuid.toString()));
